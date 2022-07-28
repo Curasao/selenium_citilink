0f2755ebbe52bfc32631f3b775f11ff96c19f2e9
Curasao <you@example.com>
2022-07-28 20:43:45 +0300

    citilink unit

---
+++ поиск_смартфонов.py
@@ -0,0 +1,25 @@
+import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+link = "https://www.citilink.ru"
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+    button = browser.find_element(By.CSS_SELECTOR, ".MainHeader__search-open")
+    button.click()
+    time.sleep(10)
+    search = browser.find_element(By.TAG_NAME,"input")
+
+    # search request for OZON.ru
+    search.send_keys('Смартфон')
+    search.send_keys(Keys.RETURN)
+
+
+    time.sleep(10)
+finally:
+    # закрываем браузер после всех манипуляций
+    browser.quit()
